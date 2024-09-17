a7a5346867fa04742a9391a025edd8bc6ba52468
hassaan1shakil <112756561+hassaan1shakil@users.noreply.github.com>
2024-09-17 20:03:43 +0500

    Add files via upload

---
+++ JavaScript/Introduction/script.js
@@ -0,0 +1,18 @@
+alert("Hello World");
+
+console.log("Code has been rendered");
+
+var a = prompt("Enter your roll no.");
+
+console.log("The input number is " + a);
+
+var confirmation = confirm("Are you sure you want to leave this page?");
+
+if (confirmation){
+    console.log("Bye Bye");
+}
+
+else{
+    console.log("Thanks for staying");
+}
+
