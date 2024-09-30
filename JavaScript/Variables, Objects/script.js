8acf7a6659befc982c6602ebf317051972ff93ac
hassaan1shakil <112756561+hassaan1shakil@users.noreply.github.com>
2024-09-30 22:08:52 +0500

    Add files via upload

---
+++ JavaScript/Variables, Objects/script.js
@@ -0,0 +1,37 @@
+console.log("Heyyyy wassuppp")
+
+var a = 5;
+var b = 6;
+
+console.log(a + b);
+
+console.log(typeof a);
+
+const num1 = 5;     // const cannont be changed obviously
+
+let flag = true;
+
+if (flag)
+    console.log("Flag is Set")
+
+
+// Data Types
+
+let x1 = undefined;
+let x2 = null;          // NULL itself is considered a Primitive Datatype but the typeof NULL is an Object
+let x3 = 22;
+let x4 = "string";
+let x5 = true;
+
+
+// Objects
+
+let obj = {
+
+    "name": "hassaan",
+    "id": 6544
+};
+
+obj.clearance = "level-3";
+
+console.log(obj);
